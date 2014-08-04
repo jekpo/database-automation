7a57020150eda057a99b97f0ba33d3c2d8979a7f
Israel Ekpo <june.ekpo@gmail.com>
2014-08-04 17:18:17 -0400

    Saving changes

---
+++ src/main/java/com/ekpo/june/data/validation/core/Employee.java
@@ -0,0 +1,5 @@
+package com.ekpo.june.data.validation.core;
+
+public class Employee {
+
+}
---
+++ src/test/java/com/ekpo/june/data/validation/test/SampleTest.java
@@ -0,0 +1,11 @@
+package com.ekpo.june.data.validation.test;
+
+import org.junit.Test;
+
+public class SampleTest {
+
+    @Test
+    public void sampleValidator(){
+
+    }
+}
